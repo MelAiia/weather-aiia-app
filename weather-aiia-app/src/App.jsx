f7b977aaa46eb7406989582a7e68b91ea3646eee
MelAiia <shavlakayya@gmail.com>
2026-07-09 12:30:04 +0300

    layout

--- weather-aiia-app/src/App.jsx
+++ weather-aiia-app/src/App.jsx
@@ -1,10 +1,29 @@
 import "./App.css";
 
+import Header from "./components/Header";
+import SearchForm from "./components/SearchForm";
+import CurrentWeather from "./components/CurrentWeather";
+import WeatherSidebar from "./components/WeatherSidebar";
+import Forecast from "./components/Forecast";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <>
-      <h1>Weather Dashboard</h1>
-    </>
+    <div className="app">
+      <div className="weather-card">
+        <Header />
+        <SearchForm />
+
+        <div className="weather-content">
+          <CurrentWeather />
+          <WeatherSidebar />
+        </div>
+
+        <Forecast />
+
+        <Footer />
+      </div>
+    </div>
   );
 }
 
---
+++ weather-aiia-app/src/components/WeatherSidebar.jsx
@@ -0,0 +1,5 @@
+function WeatherSidebar() {
+  return <div>Sidebar</div>;
+}
+
+export default WeatherSidebar;
