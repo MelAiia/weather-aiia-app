bd7520fb0cb736db7de55d831399367e37d79c51
MelAiia <shavlakayya@gmail.com>
2026-07-09 13:42:13 +0300

    Added Header jsx and css

--- weather-aiia-app/src/components/Header/Header.jsx
+++ weather-aiia-app/src/components/Header/Header.jsx
@@ -1,5 +1,20 @@
+import "./Header.css";
+import { WiCloud } from "react-icons/wi";
+import { FiSettings } from "react-icons/fi";
+
 function Header() {
-  return <header>Header</header>;
+  return (
+    <header className="header">
+      <div className="logo">
+        <WiCloud className="logo-icon" />
+        <span>Skye</span>
+      </div>
+
+      <button className="settings-button" aria-label="Settings">
+        <FiSettings />
+      </button>
+    </header>
+  );
 }
 
 export default Header;
