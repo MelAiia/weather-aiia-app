cd98c8ce586d49cbd0e330978f8ab9840ae1c673
MelAiia <shavlakayya@gmail.com>
2026-07-09 13:50:28 +0300

    changed imports for App.jsx

--- weather-aiia-app/src/App.jsx
+++ weather-aiia-app/src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 
-import Header from "./components/Header";
-import SearchForm from "./components/SearchForm";
-import CurrentWeather from "./components/CurrentWeather";
-import WeatherSidebar from "./components/WeatherSidebar";
-import Forecast from "./components/Forecast";
-import Footer from "./components/Footer";
+import Header from "./components/Header/Header";
+import SearchForm from "./components/SearchForm/SearchForm";
+import CurrentWeather from "./components/CurrentWeather/CurrentWeather";
+import WeatherSidebar from "./components/WeatherSidebar/WeatherSidebar";
+import Forecast from "./components/Forecast/Forecast";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
