27b74095455666c89effb8b74cb3560b60055539
MelAiia <shavlakayya@gmail.com>
2026-07-09 13:43:42 +0300

    Added SearchForm jsx and css

--- weather-aiia-app/src/components/SearchForm/SearchForm.jsx
+++ weather-aiia-app/src/components/SearchForm/SearchForm.jsx
@@ -1,5 +1,21 @@
+import "./SearchForm.css";
+import { FiSearch } from "react-icons/fi";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+
 function SearchForm() {
-  return <div>Search Form</div>;
+  return (
+    <form className="search-form">
+      <div className="search-input-wrapper">
+        <FiSearch className="search-icon" />
+
+        <input type="search" placeholder="Search for a city..." />
+      </div>
+
+      <button type="button" className="location-button">
+        <HiOutlineLocationMarker />
+      </button>
+    </form>
+  );
 }
 
 export default SearchForm;
