4af2e1c90f25beb329863facf3177f973c017a9e
MelAiia <shavlakayya@gmail.com>
2026-07-10 15:12:53 +0300

    Changed CurrentWeather jsx and css

--- weather-aiia-app/src/components/CurrentWeather/CurrentWeather.jsx
+++ weather-aiia-app/src/components/CurrentWeather/CurrentWeather.jsx
@@ -1,7 +1,30 @@
 import "./CurrentWeather.css";
+import { WiDayRain } from "react-icons/wi";
 
 function CurrentWeather() {
-  return <div className="current-weather">Current Weather</div>;
+  return (
+    <section className="current-weather">
+      <div className="weather-top">
+        <h1 className="city">Kyiv, Ukraine</h1>
+
+        <p className="date">Friday • 10 July 2026</p>
+
+        <p className="description">Light Rain</p>
+      </div>
+
+      <div className="weather-icon">
+        <WiDayRain />
+      </div>
+
+      <div className="weather-bottom">
+        <h2 className="temperature">23°</h2>
+
+        <p className="feels-like">
+          Feels like <strong>21°</strong>
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default CurrentWeather;
