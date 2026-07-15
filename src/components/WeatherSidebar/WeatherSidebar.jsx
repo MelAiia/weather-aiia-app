f0ba51defdc6a654ed504541152467e5c27de07e
MelAiia <shavlakayya@gmail.com>
2026-07-15 14:56:06 +0300

    Added new function for sidebar

--- src/components/WeatherSidebar/WeatherSidebar.jsx
+++ src/components/WeatherSidebar/WeatherSidebar.jsx
@@ -2,6 +2,25 @@ import "./WeatherSidebar.css";
 
 import { WiDaySunny, WiSunrise, WiSunset, WiRain } from "react-icons/wi";
 
+function formatTime(timestamp) {
+  return new Date(timestamp * 1000).toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+}
+
+function getPrecipitationDescription(value) {
+  if (value === 0) return "No rain";
+
+  if (value < 1) return "Very light rain";
+
+  if (value < 3) return "Light rain";
+
+  if (value < 8) return "Moderate rain";
+
+  return "Heavy rain";
+}
+
 function WeatherSidebar({ weather }) {
   return (
     <aside className="weather-sidebar">
@@ -20,9 +39,9 @@ function WeatherSidebar({ weather }) {
 
         <span className="info-label">Sunrise</span>
 
-        <strong className="info-value">05:12</strong>
+        <strong className="info-value">{formatTime(weather.sunrise)}</strong>
 
-        <span className="info-description">Morning begins</span>
+        <span className="info-description">Sunrise</span>
       </div>
 
       <div className="info-card">
@@ -30,7 +49,7 @@ function WeatherSidebar({ weather }) {
 
         <span className="info-label">Sunset</span>
 
-        <strong className="info-value">20:49</strong>
+        <strong className="info-value">{formatTime(weather.sunset)}</strong>
 
         <span className="info-description">Golden hour</span>
       </div>
@@ -40,9 +59,11 @@ function WeatherSidebar({ weather }) {
 
         <span className="info-label">Precipitation</span>
 
-        <strong className="info-value">20%</strong>
+        <strong className="info-value"> {weather.precipitation} mm</strong>
 
-        <span className="info-description">Light rain expected</span>
+        <span className="info-description">
+          {getPrecipitationDescription(weather.precipitation)}
+        </span>
       </div>
     </aside>
   );
